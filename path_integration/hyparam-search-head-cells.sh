cecd361de7898edb31018c53edbf6911fcbb5770
mandanasmi <samiemandana@gmail.com>
2020-04-16 18:37:18 -0400

    Updates GridTorch model for a hyperparameter search

--- path_integration/hyparam-search-head-cells.sh
+++ path_integration/hyparam-search-head-cells.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+
+
+tmp_date=experiments/results/hyparam_search_head_cells/$(date '+%Y-%m-%d_%H-%M')/
+mkdir $tmp_date
+
+python ./train.py --num_epochs 1000 --save_dir $tmp_date --num_place_cells=256 --num_headD_cells=24
+python ./train.py --num_epochs 1000 --save_dir $tmp_date --num_place_cells=256 --num_headD_cells=36
+python ./train.py --num_epochs 1000 --save_dir $tmp_date --num_place_cells=256 --num_headD_cells=48
